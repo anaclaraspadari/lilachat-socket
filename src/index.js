7699ff3bea385c5a443813b426e741ffc6f754ef
Ana Clara Cardozo <38481468+anaclaraspadari@users.noreply.github.com>
2022-07-07 12:16:18 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,48 @@
+const express=require('express');
+const app=express();
+const http=require('http');
+const server=http.createServer(app);
+const { Server }=require('socket.io');
+const io=new Server(server);
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname+'/index.html');
+});
+
+io.on('connection', (socket) => {
+    socket.client.nick = socket.client.id;
+    console.log('usuario conectado');
+    let date=new Date();
+    date=`${date.getFullYear()}/${date.getMonth()+1}/${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    //console.log(date);
+    io.emit('chat message', '['+date+']'+' '+ socket.client.nick + " entrou");
+
+    socket.on('chat message', (msg) => {
+        let date=new Date();
+        date=`${date.getFullYear()} / ${date.getMonth()} / ${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+        console.log('sid: ' + socket.client.id + '\tmessage: ' + msg);
+        io.emit('chat message', '['+date+']'+' '+ socket.client.nick + " disse: " + msg);
+    });
+
+    socket.on('set nick', (msg) => {
+        const oldNick = socket.client.nick
+        io.emit('chat message', `${oldNick} trocou seu nome para ${msg}`);
+        socket.client.nick = msg;
+    });
+
+    socket.on('typing', (msg) => {
+        console.log(msg);
+        io.emit('typing', `${socket.client.nick} ${msg}`);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('usuario desconectado');
+        let date=new Date();
+        date=`${date.getFullYear()}/${date.getMonth()}/${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+        io.emit('chat message', '['+date+']'+' '+ socket.client.nick + " saiu");
+    });
+});
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
